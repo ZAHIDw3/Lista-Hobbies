76d094e956867037e3d79534b48a68d60bc25063
Zahid de Jesus Avalos Hernandez <l19350283@tuctepec.tecnm.mx>
2023-03-01 09:25:07 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const nombre = "Zahid de Jesus Avalos Hernandez"
+  return (
+    <div>
+      <h3>Lista de Hobbies {nombre}</h3>
+      <ol>
+        <li>Leer manga</li>
+        <li>Ver anime</li>
+        <li>Dormir</li>
+      </ol></div>
+  )
+}
+
+export default App
